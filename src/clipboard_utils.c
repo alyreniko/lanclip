c75e0b4fa2eac6155cf892008ef1948f6eeb95d5
Хайдаров Диёр <dyrhyrv@ya.ru>
2025-04-14 18:18:04 +0700

    feat(clipboard): add clipboard support for X11 via xclip and detect session type

--- src/clipboard_utils.c
+++ src/clipboard_utils.c
@@ -2,11 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-void get_clipboard_wayland(char *dest, size_t max_len) {
-    FILE *fp = popen("wl-paste", "r");
+void get_clipboard(char *dest, size_t max_len) {
+    const char *session_type = getenv("XDG_SESSION_TYPE");
+    const char *cmd = NULL;
+
+    if (session_type && strcmp(session_type, "wayland") == 0) {
+        cmd = "wl-paste";
+    } else {
+        cmd = "xclip -selection clipboard -o";
+    }
+
+    FILE *fp = popen(cmd, "r");
     if (fp == NULL) {
         return;
     }
+
     fgets(dest, max_len, fp);
     pclose(fp);
 }
